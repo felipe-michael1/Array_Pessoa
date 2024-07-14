c72ed0bf98e1338ae918b022da9ea384cdb3afc2
Felipe Michael da Fonseca <felipe.fonseca.michael1@gmail.com>
2024-07-14 16:02:36 -0300

    Create teste.php

---
+++ teste.php
@@ -0,0 +1,28 @@
+<?php
+// criado por Felipe Michael da Fonseca,
+// Array que retorna todas as pessoas
+//Criado em: 14/07/2024
+
+ function array_pessoa () { 	
+   	$array_pessoa = array(
+	   array(
+	      'idade' => '25',
+	      'nome' => 'joão',
+                       'cidade' => 'São Francisco',
+	      'país' => 'Estados Unidos'
+                    ),
+	   array(
+	     'idade' => '40',
+	     'nome' => 'Mariazinha',
+                      'cidade' => 'Nova York',
+	     'país' => 'Estados Unidos'
+                   )
+               );
+    
+    	$resultado_final = json.encode($array_pessoa);
+    	return $resultado_final;
+ }
+
+$retorna_array = array_pessoa();
+var_dump($retorna_array);
+?>
